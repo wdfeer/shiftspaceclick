50fbc47c6edff0195da362895bd2a00bb98b163f
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-06-06 19:52:38 +0200

    Implement render?

--- internal/render.go
+++ internal/render.go
@@ -4,4 +4,32 @@ import rl "github.com/gen2brain/raylib-go/raylib"
 
 func (state State) Render() {
 	rl.ClearBackground(rl.Black)
+
+	state.Player.render()
+
+	for _, e := range state.Enemies {
+		e.render()
+	}
+
+	for _, p := range state.Projectiles {
+		p.render()
+	}
+}
+
+func (player Player) render() {
+	rl.DrawCircleV(player.Position, player.Size/2, rl.RayWhite)
+}
+
+func (enemy Enemy) render() {
+	rl.DrawCircleV(enemy.Position, enemy.Size/2, rl.Maroon)
+}
+
+func (projectile Projectile) render() {
+	var color rl.Color
+	if projectile.Hostile {
+		color = rl.Red
+	} else {
+		color = rl.White
+	}
+	rl.DrawCircleV(projectile.Position, projectile.Size/2, color)
 }
