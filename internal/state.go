e6e9c2476e57b7763a67d25e49ef1ab6bf118db1
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-06-06 19:33:29 +0200

    Implement independent parallelized updates

--- internal/state.go
+++ internal/state.go
@@ -1,5 +1,28 @@
 package internal
 
+import rl "github.com/gen2brain/raylib-go/raylib"
+
 type State struct {
-	// TODO
+	Player      Player
+	Enemies     EnemyList
+	Projectiles ProjectileList
+}
+
+type Player struct {
+	Position rl.Vector2
+	Velocity rl.Vector2
+}
+
+type EnemyList = [99]Enemy
+type Enemy struct {
+	Alive    bool
+	Position rl.Vector2
+}
+
+type ProjectileList = [999]Projectile
+type Projectile struct {
+	Alive    bool
+	Position rl.Vector2
+	Velocity rl.Vector2
+	Hostile  bool
 }
--- internal/update.go
+++ internal/update.go
@@ -1,6 +1,83 @@
 package internal
 
+import (
+	"sync"
+
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
+
 func (state State) Update() State {
-	// TODO
+	state = handleIndependent(state)
+	handleInteractions(&state)
 	return state
+}
+
+func handleIndependent(state State) State {
+	var (
+		newPlayer      Player
+		newEnemies     EnemyList
+		newProjectiles ProjectileList
+	)
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	go func() {
+		defer wg.Done()
+		newPlayer = updatePlayer(state.Player)
+	}()
+	go func() {
+		defer wg.Done()
+		newEnemies = updateEnemies(state)
+	}()
+	go func() {
+		defer wg.Done()
+		newProjectiles = updateProjectiles(state.Projectiles)
+	}()
+
+	return State{newPlayer, newEnemies, newProjectiles}
+}
+
+func updatePlayer(player Player) Player {
+	inputMap := map[int32]rl.Vector2{rl.KeyW: {X: 1, Y: 0}}
+	newVelocity := player.Velocity
+	for key, direction := range inputMap {
+		if rl.IsKeyDown(key) {
+			newVelocity = rl.Vector2Add(newVelocity, direction)
+		}
+	}
+	return Player{
+		rl.Vector2Add(player.Position, player.Velocity),
+		newVelocity,
+	}
+}
+
+func updateEnemies(state State) EnemyList {
+	newEnemies := EnemyList{}
+	for i, e := range state.Enemies {
+		if e.Alive {
+			newPos := rl.Vector2MoveTowards(e.Position, state.Player.Position, 20)
+			newEnemies[i] = Enemy{true, newPos}
+		}
+	}
+	return newEnemies
+}
+
+func updateProjectiles(projectiles ProjectileList) ProjectileList {
+	newProjectiles := ProjectileList{}
+	for i, p := range projectiles {
+		if p.Alive {
+			newProjectiles[i] = Projectile{
+				Alive:    true,
+				Position: rl.Vector2Add(p.Position, p.Velocity),
+				Velocity: p.Velocity,
+				Hostile:  p.Hostile,
+			}
+		}
+	}
+	return newProjectiles
+}
+
+func handleInteractions(state *State) {
+	// TODO: update collisions, hp, spawning projectiles/enemies
 }
